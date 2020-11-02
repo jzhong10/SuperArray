b80a25fa24b955b6c6b5525ebb18eeb40cdce193
jzhong10 <junzhong2003@gmail.com>
2020-11-02 17:35:37 -0500

    Tested and implemented SuperArray compiler and size() method

---
+++ SuperArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SuperArray {
+    private String[] data;
+    private int size;
+
+    public SuperArray() {
+      data = new String[10];
+      size = 10;
+    }
+
+    public int size() {
+      return size;
+    }
+}
---
+++ Tester.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Tester {
+    public static void main(String[] args) {
+      SuperArray a1 = new SuperArray();
+      System.out.println(a1.size());
+    }
+}
