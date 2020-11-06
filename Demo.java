9031a33e72d7ea9804e622d9382012e337808f30
jzhong10 <junzhong2003@gmail.com>
2020-11-06 18:27:18 -0500

    Tested and implemented findOverlap method

--- Demo.java
+++ Demo.java
@@ -14,15 +14,39 @@ public class Demo {
     }
   }
 
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    removeDuplicates(a);
+    removeDuplicates(b);
+    SuperArray sarr = new SuperArray();
+    for (int i = 0; i < a.size(); i++) {
+      if (b.contains(a.get(i))) {
+        sarr.add(a.get(i));
+      }
+    }
+    return sarr;
+  }
+
   public static void main(String[] args) {
     SuperArray words = new SuperArray();
     //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-    words.add("una");    words.add("ebi");     words.add("toro");
+    words.add("uhuh");    words.add("56256");     words.add("toro");
 
     System.out.println(words);
-    removeDuplicates(words);
+    //removeDuplicates(words);
     System.out.println(words);
+
+    SuperArray words1 = new SuperArray();
+    words1.add("kani");   words1.add("uni");     words1.add("oidjf 9i2qu");     words1.add("una");
+    words1.add("uhuh");    words1.add("ebi");     words1.add("7ouiu8");    words1.add("una");
+    words1.add("una");    words1.add("ebi");     words1.add("toro");
+
+    System.out.println(words1);
+    //removeDuplicates(words1);
+    System.out.println(words1);
+
+    SuperArray worded = findOverlap(words, words1);
+    System.out.println(worded);
   }
 }
